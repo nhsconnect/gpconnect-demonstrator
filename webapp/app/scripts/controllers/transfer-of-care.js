b5219dcb844cf12d98297f41a55c4c5d060cb015
joehoughton <joehoughton93@gmail.com>
2015-03-21 23:00:41 +0000

    Added transfer of care detail and confirmation modal. transferOfCare object now updated.

--- webapp/app/scripts/controllers/transfer-of-care.js
+++ webapp/app/scripts/controllers/transfer-of-care.js
@@ -29,44 +29,59 @@ angular.module('openehrPocApp')
         $scope.problems = $scope.transferOfCare.problems.problems;
         $scope.medications = $scope.transferOfCare.medication.medications;
 
-        var selectedItems = {
+        $scope.selectedItems = {
           problems:[],
           medications:[],
           allergies:[],
           contacts:[]
         }
 
-        $scope.selecteditemzzz = selectedItems; //REMOVE THIS
-
         $scope.selectTransferOfCareItem = function(selectedIndex,type){
           //console.log("type passed " + type);
           //console.log("selected:" + selectedIndex);
-          selectedItems[type].indexOf(selectedIndex) != -1 ? selectedItems[type].splice(selectedItems[type].indexOf(selectedIndex),1) : selectedItems[type].push(selectedIndex);
+          $scope.selectedItems[type].indexOf(selectedIndex) != -1 ? $scope.selectedItems[type].splice($scope.selectedItems[type].indexOf(selectedIndex),1) : $scope.selectedItems[type].push(selectedIndex);
           //console.log("UPDATED selected:");
           //console.log(selectedItems[type]);
         };
 
         $scope.isItemSelected = function(index,type){
-        return selectedItems[type].indexOf(index) != -1 ? "transfer-of-care-green" : "transfer-of-care-red";
+            return $scope.selectedItems[type].indexOf(index) != -1 ? "transfer-of-care-green" : "transfer-of-care-red";
         }
 
         $scope.isItemSelectedIcon = function(index,type){
-          return selectedItems[type].indexOf(index) != -1 ? "glyphicon glyphicon-ok" : "glyphicon glyphicon-remove";
+            return $scope.selectedItems[type].indexOf(index) != -1 ? "glyphicon glyphicon-ok" : "glyphicon glyphicon-remove";
         }
+
+        $scope.getSelectedItItemsForSummary = function(transferOfCareForm) {
+
+          $scope.formSubmitted = true; //is this necessary - ask phil?
+
+          if (transferOfCareForm.$valid) { <!--Fix - display errors when invalid-->
+            $scope.selectedItemsForSummary = updateTransferOfCare();
+            $scope.selectedAllergies = $scope.selectedItemsForSummary.allergies.allergies;
+            $scope.selectedContacts = $scope.selectedItemsForSummary.contacts.contacts;
+            $scope.selectedProblems = $scope.selectedItemsForSummary.problems.problems;
+            $scope.selectedMedications = $scope.selectedItemsForSummary.medication.medications;
+            $scope.toggleDetailView();
+          }
+
+        };
 
         function updateTransferOfCare(){
 
-          for (var type in selectedItems) {
+        var updatedTransferOfCare = $.extend( true, {}, $scope.transferOfCare );
+
+          for (var type in $scope.selectedItems) {
 
             switch (type){
               case 'allergies':
               case 'problems':
               case 'contacts':
 
-                for (var transferIndex = $scope.transferOfCare[type][type].length; transferIndex--;) {
+                for (var transferIndex = updatedTransferOfCare[type][type].length; transferIndex--;) {
 
                   var contains = false;
-                  angular.forEach(selectedItems[type], function (value) {
+                  angular.forEach($scope.selectedItems[type], function (value) {
                     //console.log("selected type: " + value);
                     var selectedItemIndex = value;
 
@@ -77,7 +92,7 @@ angular.module('openehrPocApp')
 
                   if (contains === false) {
                     //console.log("Transfer does NOT contain " + transferIndex);
-                    $scope.transferOfCare[type][type].splice(transferIndex, 1);
+                    updatedTransferOfCare[type][type].splice(transferIndex, 1);
                    // console.log("splice " + transferIndex);
                   } else {
                    // console.log("Transfer does contain " + transferIndex);
@@ -86,10 +101,10 @@ angular.module('openehrPocApp')
                 }
                     break;
               case 'medications':
-                for (var transferIndex = $scope.transferOfCare['medication'][type].length; transferIndex--;) {
+                for (var transferIndex = updatedTransferOfCare['medication'][type].length; transferIndex--;) {
 
                   var contains = false;
-                  angular.forEach(selectedItems[type], function (value) {
+                  angular.forEach($scope.selectedItems[type], function (value) {
                     var selectedItemIndex = value;
 
                     if (transferIndex == selectedItemIndex) {
@@ -98,59 +113,46 @@ angular.module('openehrPocApp')
                   });
 
                   if (contains === false) {
-                    $scope.transferOfCare['medication'][type].splice(transferIndex, 1);
+                    updatedTransferOfCare['medication'][type].splice(transferIndex, 1);
                   }
 
                 }
                     break;
             }
           }
-        return $scope.transferOfCare;
-        }
 
-       /* $scope.dismiss = function () {
-          $scope.$dismiss();
+        return updatedTransferOfCare;
         };
 
-        $scope.save = function () {
-          $scope.$close(true);
-        };*/
+        $scope.displayDetail = false;
+        $scope.toggleDetailView = function() {
+            $scope.displayDetail = !$scope.displayDetail;
+        };
 
-        $scope.ok = function (form, toc) {
+        $scope.dismiss = function () {
+            $scope.$dismiss();
+        };
 
-          $scope.formSubmitted = true;
-          if (form.$valid) {
-
-            $scope.$close(toc);
-
-             var result = updateTransferOfCare();
-             console.log(result);
-            //alert("returned!")
-
-          }
+         $scope.ok = function () {
+            $scope.transferOfCare = updateTransferOfCare();
+            console.log("The final transferOfCare object:")
+            console.log($scope.transferOfCare);
+            $scope.$close();
         };
 
       }
     }).result.then(function () {
-
         confirmTransferOfCareSelections();
-
     });
 
     function confirmTransferOfCareSelections() {
-      alert("confirmTransferOfCareSelections called");
-
-      $modal.open({
+        $modal.open({
         templateUrl: 'views/transfer-of-care/transfer-of-care-confirmation.html',
         size: 'md',
         controller: function ($scope) {
 
           $scope.ok = function () {
-            $scope.close();
-          };
-
-          $scope.cancel = function () {
-            alert("needs to go back to first modal");
+             $scope.$close();
           };
 
         }

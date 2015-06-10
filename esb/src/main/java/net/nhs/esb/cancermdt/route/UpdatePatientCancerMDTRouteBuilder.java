bf2f208d1e702de321cefb19b7b0f11c174736c7
CoxJ <CoxJ@ANS-A312.upco.co.uk>
2015-06-10 15:27:29 +0100

    Moved the composition in the header to stop it being sent to openEHR as it made the header to large and the server would not accept the request.

--- esb/src/main/java/net/nhs/esb/cancermdt/route/UpdatePatientCancerMDTRouteBuilder.java
+++ esb/src/main/java/net/nhs/esb/cancermdt/route/UpdatePatientCancerMDTRouteBuilder.java
@@ -26,19 +26,31 @@ public class UpdatePatientCancerMDTRouteBuilder extends SpringRouteBuilder {
         from("direct:updatePatientCancerMDTComposition").routeId("openEhrUpdatePatientCancerMDTComposition")
                 .setHeader("compositionId", simple("${body.compositionId}"))
                 .convertBodyTo(CancerMDTUpdate.class)
-                .setHeader("composition", simple("${body.content}"))
+                
+                // Store composition into a Camel* header rather than "composition" so it is not sent to openEHR as it is too big for openEHR to accept
+                //.setHeader("composition", simple("${body.content}"))
+                .setHeader("Camel.openEHR.composition", simple("${body.content}"))
                 .to("direct:openEhrUpdatePatientCancerMDTComposition");
 
         from("direct:openEhrUpdatePatientCancerMDTComposition")
+                
                 .to("direct:setHeaders")
                 .to("direct:createSession")
                 .to("direct:getEhrId")
+                
+                // Switch composition back into "composition" header for standard processor
+                .setHeader("composition", simple("${header.Camel.openEHR.composition}"))
+                .removeHeaders("Camel.openEHR.composition")
+                
                 .setExchangePattern(ExchangePattern.InOut)
                 .setHeader(CxfConstants.CAMEL_CXF_RS_USING_HTTP_API, constant(Boolean.FALSE))
                 .setHeader(CxfConstants.OPERATION_NAME, constant("updateComposition"))
                 .setHeader("template", constant(cancerMDTTemplate))
                 .bean(compositionUpdateParameters)
+                
+                // Now the composition header details are back in the body the header needs removing or the header will be two large for openEHR
+                .removeHeaders("composition")
                 .to("cxfrs:bean:rsOpenEhr")
                 .end();
-    } 
+    }
 }

a166d70a7a663e95a9aaac612cfc066680d321c0
Iain Rawson <iain.rawson@answerconsulting.com>
2015-07-29 08:24:38 +0100

    Commented out end of life code so that it produces dummy data for the demo

--- esb/src/main/java/net/nhs/esb/endoflife/route/FindEndOfLifeRouteBuilder.java
+++ esb/src/main/java/net/nhs/esb/endoflife/route/FindEndOfLifeRouteBuilder.java
@@ -1,11 +1,14 @@
 package net.nhs.esb.endoflife.route;
 
+import java.util.Collections;
+
+import net.nhs.esb.endoflife.model.CareDocument;
+import net.nhs.esb.endoflife.model.CprDecision;
+import net.nhs.esb.endoflife.model.EndOfLifeCarePlan;
 import net.nhs.esb.endoflife.model.EndOfLifeComposition;
+import net.nhs.esb.endoflife.model.PrioritiesOfCare;
+import net.nhs.esb.endoflife.model.TreatmentDecision;
 import net.nhs.esb.openehr.route.CompositionSearchParameters;
-import net.nhs.esb.util.DefaultAggregationStrategy;
-import net.nhs.esb.util.EmptyList;
-import org.apache.camel.ExchangePattern;
-import org.apache.camel.component.cxf.common.message.CxfConstants;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,11 +21,16 @@ public class FindEndOfLifeRouteBuilder extends SpringRouteBuilder {
     @Autowired
     private CompositionSearchParameters compositionParameters;
 
+    private static final DummyData dummyData = new DummyData();
+
     @Override
     public void configure() throws Exception {
 
         //@formatter:off
         from("direct:findPatientEndOfLifeCarePlan").routeId("FindPatientEndOfLifeCarePlan")
+// TODO Remove lines below and restore commented out code
+            .bean(dummyData, "dummyData")
+/*
             .to("direct:setHeaders")
             .to("direct:createSession")
             .to("direct:getEhrId")
@@ -44,6 +52,7 @@ public class FindEndOfLifeRouteBuilder extends SpringRouteBuilder {
                 .otherwise()
                     .removeHeader("CamelHttpResponseCode")
                     .setBody(new EmptyList())
+*/
         .end();
         //@formatter:on
     }
@@ -53,5 +62,44 @@ public class FindEndOfLifeRouteBuilder extends SpringRouteBuilder {
                 "from EHR e[ehr_id/value='${header.ehrId}'] " +
                 "contains COMPOSITION a[openEHR-EHR-COMPOSITION.care_plan.v1] " +
                 "where a/name/value='End of Life Patient Preferences'";
+    }
+
+    public static class DummyData {
+        public EndOfLifeComposition dummyData() {
+
+            CareDocument careDocument = new CareDocument();
+            careDocument.setName("End of Life Care");
+            careDocument.setType("Document");
+            careDocument.setAuthor("Dr John Smith");
+            careDocument.setDate("2015-01-01");
+
+            CprDecision cprDecision = new CprDecision();
+            cprDecision.setCprDecision("For attempted cardio-pulmonary resuscitation");
+            cprDecision.setComment("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.");
+            cprDecision.setDateOfDecision("2015-01-02");
+
+            PrioritiesOfCare priorities = new PrioritiesOfCare();
+            priorities.setPlaceOfDeath("Home");
+            priorities.setPlaceOfCare("Leeds General");
+            priorities.setComment("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.");
+
+            TreatmentDecision treatmentDecision = new TreatmentDecision();
+            treatmentDecision.setDecisionToRefuseTreatment("Advanced decision to refuse treatment signed");
+            treatmentDecision.setDateOfDecision("2015-01-03");
+            treatmentDecision.setComment("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.");
+
+            EndOfLifeCarePlan carePlan = new EndOfLifeCarePlan();
+            carePlan.setSource("openehr");
+            carePlan.setCareDocument(careDocument);
+            carePlan.setCprDecision(cprDecision);
+            carePlan.setPrioritiesOfCare(priorities);
+            carePlan.setTreatmentDecision(treatmentDecision);
+
+            EndOfLifeComposition endOfLifeComposition = new EndOfLifeComposition();
+            endOfLifeComposition.setCompositionId("dummy");
+            endOfLifeComposition.setEolCarePlans(Collections.singletonList(carePlan));
+
+            return endOfLifeComposition;
+        }
     }
 }

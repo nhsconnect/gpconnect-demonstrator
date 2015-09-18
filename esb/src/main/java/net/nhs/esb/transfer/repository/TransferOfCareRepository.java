562354de46f19b373c1c3803778d7205e0b5ee64
Josh.Hilland <josh.hilland@answerconsulting.com>
2015-09-18 16:33:12 +0100

    Deleted re-implemented files from the old and unused esb package

--- esb/src/main/java/net/nhs/esb/transfer/repository/TransferOfCareRepository.java
+++
@@ -1,9 +0,0 @@
-package net.nhs.esb.transfer.repository;
-
-import net.nhs.esb.transfer.model.TransferOfCare;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransferOfCareRepository extends JpaRepository<TransferOfCare, Long> {
-
-}

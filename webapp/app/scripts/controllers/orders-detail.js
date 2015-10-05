c1e04d489bc5aef76461cf9208b0da8e0ccdc5f0
Dickson Lui <dickson.lui@answerconsulting.com>
2015-10-05 17:06:25 +0100

    Removed unused code with Orders edit

--- webapp/app/scripts/controllers/orders-detail.js
+++ webapp/app/scripts/controllers/orders-detail.js
@@ -11,31 +11,4 @@ angular.module('rippleDemonstrator')
       $scope.order = result.data;
     });
 
-    $scope.edit = function () {
-      var modalInstance = $modal.open({
-        templateUrl: 'views/orders/orders-modal.html',
-        size: 'lg',
-        controller: 'OrdersModalCtrl',
-        resolve: {
-          modal: function () {
-            return {
-              title: 'Edit Order'
-            };
-          },
-          order: function () {
-            return angular.copy($scope.order);
-          },
-          patient: function () {
-            return $scope.patient;
-          }
-        }
-      });
-
-      modalInstance.result.then(function (order) {
-        Order.update($scope.patient.id, order).then(function () {
-          $location.path('/patients/' + $scope.patient.id + '/orders');
-        });
-      });
-    };
-
   });

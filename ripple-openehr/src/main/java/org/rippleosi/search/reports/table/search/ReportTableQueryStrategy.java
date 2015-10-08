97963c28314b342dce597d409c2471217b88755f
Josh.Hilland <josh.hilland@answerconsulting.com>
2015-10-08 16:25:19 +0100

    Changed and added some dummay data for front end testing on the reports table

--- ripple-openehr/src/main/java/org/rippleosi/search/reports/table/search/ReportTableQueryStrategy.java
+++ ripple-openehr/src/main/java/org/rippleosi/search/reports/table/search/ReportTableQueryStrategy.java
@@ -51,19 +51,19 @@ public class ReportTableQueryStrategy extends AbstractQueryStrategy<ReportTableR
         RecordHeadline medsHeadline = new RecordHeadline();
         medsHeadline.setSource("openehr");
         medsHeadline.setSourceId("319be2a4-8899-4098-97e3-64eca3fa3420::answer.hopd.com::2");
-        medsHeadline.setLatestEntry(DateFormatter.toDate("2015-06-08"));
+        medsHeadline.setLatestEntry(DateFormatter.toDate("2014-06-08"));
         medsHeadline.setTotalEntries("9");
 
         RecordHeadline resultsHeadline = new RecordHeadline();
         resultsHeadline.setSource("openehr");
         resultsHeadline.setSourceId("8d032f73-f0c6-47bc-a0f8-70e93fed4e67::answer.hopd.com::1");
-        resultsHeadline.setLatestEntry(DateFormatter.toDate("2015-06-09"));
+        resultsHeadline.setLatestEntry(DateFormatter.toDate("2015-01-01"));
         resultsHeadline.setTotalEntries("1");
 
         RecordHeadline treatmentsHeadline = new RecordHeadline();
         treatmentsHeadline.setSource("openehr");
         treatmentsHeadline.setSourceId("4b6631c9-9523-4ceb-890c-dc104092ddae::answer.hopd.com::1");
-        treatmentsHeadline.setLatestEntry(DateFormatter.toDate("2015-06-09"));
+        treatmentsHeadline.setLatestEntry(DateFormatter.toDate("2012-06-09"));
         treatmentsHeadline.setTotalEntries("10");
 
         ivor.setVitalsHeadline(vitalsHeadline);
@@ -72,8 +72,52 @@ public class ReportTableQueryStrategy extends AbstractQueryStrategy<ReportTableR
         ivor.setResultsHeadline(resultsHeadline);
         ivor.setTreatmentsHeadline(treatmentsHeadline);
 
+        SearchTablePatientDetails someoneElse = new SearchTablePatientDetails();
+        someoneElse.setSource("local");
+        someoneElse.setSourceId("2");
+        someoneElse.setName("Mary Gillingham");
+        someoneElse.setAddress("123 Fake Street, High Town, Farnsborough, FH12 4WQ");
+        someoneElse.setDateOfBirth(DateFormatter.toDate("1948-06-06"));
+        someoneElse.setGender("Female");
+        someoneElse.setNhsNumber("9999999010");
+
+        RecordHeadline vitalsHead = new RecordHeadline();
+        vitalsHead.setSource("openehr");
+        vitalsHead.setTotalEntries("0");
+
+        RecordHeadline ordersHead = new RecordHeadline();
+        ordersHead.setSource("openehr");
+        ordersHead.setSourceId("8832a9db-194e-45c3-a102-2d7c69b5db6f::answer.hopd.com::1");
+        ordersHead.setLatestEntry(DateFormatter.toDate("2015-06-01"));
+        ordersHead.setTotalEntries("14");
+
+        RecordHeadline medsHead = new RecordHeadline();
+        medsHead.setSource("openehr");
+        medsHead.setSourceId("319be2a4-8899-4098-97e3-64eca3fa3420::answer.hopd.com::2");
+        medsHead.setLatestEntry(DateFormatter.toDate("2014-06-08"));
+        medsHead.setTotalEntries("9");
+
+        RecordHeadline resultsHead = new RecordHeadline();
+        resultsHead.setSource("openehr");
+        resultsHead.setSourceId("8d032f73-f0c6-47bc-a0f8-70e93fed4e67::answer.hopd.com::1");
+        resultsHead.setLatestEntry(DateFormatter.toDate("2015-05-29"));
+        resultsHead.setTotalEntries("1");
+
+        RecordHeadline treatmentsHead = new RecordHeadline();
+        treatmentsHead.setSource("openehr");
+        treatmentsHead.setSourceId("4b6631c9-9523-4ceb-890c-dc104092ddae::answer.hopd.com::1");
+        treatmentsHead.setLatestEntry(DateFormatter.toDate("2013-12-30"));
+        treatmentsHead.setTotalEntries("10");
+
+        someoneElse.setVitalsHeadline(vitalsHead);
+        someoneElse.setOrdersHeadline(ordersHead);
+        someoneElse.setMedsHeadline(medsHead);
+        someoneElse.setResultsHeadline(resultsHead);
+        someoneElse.setTreatmentsHeadline(treatmentsHead);
+
         List<SearchTablePatientDetails> details = new ArrayList<>();
         details.add(ivor);
+        details.add(someoneElse);
 
         ReportTableResults results = new ReportTableResults();
         results.setPatientDetails(details);

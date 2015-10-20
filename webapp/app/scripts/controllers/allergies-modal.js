95ca29851469bd61922a3bc321a51774df370fb7
Dickson Lui <dickson.lui@answerconsulting.com>
2015-10-20 16:47:24 +0100

    Allergy modal updated to reflect new mockups

--- webapp/app/scripts/controllers/allergies-modal.js
+++ webapp/app/scripts/controllers/allergies-modal.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('rippleDemonstrator')
-  .controller('AllergiesModalCtrl', function ($scope, $modalInstance, allergy, patient, modal) {
+  .controller('AllergiesModalCtrl', function ($scope, $modalInstance, allergy, PatientService, patient, modal) {
 
+    $scope.currentUser = PatientService.getCurrentUser();
     $scope.allergy = allergy;
     $scope.patient = patient;
     $scope.modal = modal;
+
+    if (modal.title === 'Create Allergy') {
+      $scope.allergy.dateSubmitted = new Date().toISOString().slice(0, 10);
+      $scope.allergy.causeCode = '1239085';
+      $scope.allergy.terminologyCode = '12393890';
+    } else {
+      $scope.allergy.dateSubmitted = new Date($scope.allergy.dateSubmitted).toISOString().slice(0, 10);
+    }
 
     $scope.ok = function (allergyForm, allergy) {
       $scope.formSubmitted = true;

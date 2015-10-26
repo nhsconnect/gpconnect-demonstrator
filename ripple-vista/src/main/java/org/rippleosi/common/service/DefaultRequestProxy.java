8ac740878b0291af401ee6851bc4b528b8f7bc24
Josh.Hilland <josh.hilland@answerconsulting.com>
2015-10-26 13:25:24 +0000

    Skeleton code for the Vista module, incl. services and Rest response

---
+++ ripple-vista/src/main/java/org/rippleosi/common/service/DefaultRequestProxy.java
@@ -0,0 +1,53 @@
+/*
+ *   Copyright 2015 Ripple OSI
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ */
+package org.rippleosi.common.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class DefaultRequestProxy implements RequestProxy {
+
+    @Value("${vista.address}")
+    private String vistaAddress;
+
+    @Override
+    public <T> ResponseEntity<T> getWithSession(String uri, Class<T> cls) {
+        return null;
+    }
+
+    private RestTemplate restTemplate() {
+        return new RestTemplate(new SimpleClientHttpRequestFactory());
+    }
+
+    private HttpEntity<String> buildRequest(String body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        return new HttpEntity<>(body, headers);
+    }
+}

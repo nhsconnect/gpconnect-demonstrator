6f2a935c74d497bc0f0da8c0a276eb25c234901c
james-answer <james.cox@answerdigital.com>
2016-07-01 16:21:56 +0100

    GPC-100 Added angular controller for slots screen as place holder for where the fhir model to internal model mapping will be performed.

--- webapp/app/scripts/services/appointment.js
+++ webapp/app/scripts/services/appointment.js
@@ -4,26 +4,27 @@ angular.module('gpConnect')
   .factory('Appointment', function ($http) {
 
     var findAllAppointments = function (patientId) {
-      
       var response;
-      
       return $http.get('/fhir/Patient?patientId='+patientId).then(function(response) {
          return $http.get('/fhir/Patient/' + response.data.entry[0].resource.id + '/Appointment');
       });
-      
+    };
+    
+    var getScheduleOperation = function (organizationODSCode, siteODSCode, startDateTime, endDateTime) {
+      return $http.post('/fhir/$getschedule', '{ "resourceType": "Parameters", "parameter": [{ "name": "odsOrganisationCode", "valueIdentifier": { "value": "'+organizationODSCode+'" } }, { "name": "odsSiteCode", "valueIdentifier": { "value": "'+siteODSCode+'" } }, { "name": "timePeriod", "valuePeriod": { "start": "'+startDateTime+'", "end": "'+endDateTime+'" } }] }');
     };
     
     var findResourceByReference = function (resourceReference) {
-      var response;
       return $http.get('/fhir/'+resourceReference);
     };
-
+    
     var create = function (patientId, appointment) {
       return $http.post('/api/patients/' + patientId + '/appointments', appointment);
     };
 
     return {
       findAllAppointments: findAllAppointments,
+      getScheduleOperation: getScheduleOperation,
       findResourceByReference: findResourceByReference,
       create: create
     };

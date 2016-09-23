ac3de175491f9543665fdb66fe64e8cd89331456
wweatherill-answer <will.weatherill@answerconsulting.com>
2016-09-23 13:50:25 +0100

    GPC-165 - Corrected issue where only appointments for one practice were being searched for.

--- webapp/app/scripts/controllers/appointments-controller.js
+++ webapp/app/scripts/controllers/appointments-controller.js
@@ -20,22 +20,54 @@ angular.module('gpConnect')
             initAppointments();
             
             function initAppointments() {
-            	var patientIdPromises = [];
-            	
             	$scope.missingPractices = [];
+            	$scope.foundPractices = [];
             	var allPractices = ProviderRouting.practices;
-            	var appointmentsPromises = [];
             	var practiceMap = {};
+            	
+            	var appointmentsMap = {};
             	
             	for(var practiceIndex in allPractices) {
                 	var practice = allPractices[practiceIndex];
                 	practiceMap[practice.ASID] = practice;
                 	            		
-                	var practiceOdsCode = practice.odsCode;
-                	var patientIdPromise = PatientService.getPatientFhirId($stateParams.patientId, practiceOdsCode).then(
+                	PatientService.getFhirPatientResponse($stateParams.patientId, practice.odsCode).then(
             			function(getPatientFhirIdSuccess) {
-            				var appointmentsPromise = Appointment.findAllAppointments($stateParams.patientId, getPatientFhirIdSuccess, practiceOdsCode);
-            				appointmentsPromises.push(appointmentsPromise);
+            				// lookup ODS code
+            				var sspTo = getPatientFhirIdSuccess.config.headers['Ssp-To'];
+            				var practice = practiceMap[sspTo]
+            				
+            				$scope.foundPractices.push(practiceMap[sspTo]);
+            				
+            				var patient = PatientService.getPatientFromResponse(getPatientFhirIdSuccess);
+            				
+            				Appointment.findAllAppointments($stateParams.patientId, patient.id, practice.odsCode).then(
+        						function(findAllAppointmentsSuccess) {
+        							var appointments = findAllAppointmentsSuccess.data.entry;
+                        			
+                        			if(appointments != undefined && appointments.length > 0) {
+                        				setCancellationReason(appointments);
+                        				var sspTo = findAllAppointmentsSuccess.config.headers['Ssp-To'];
+                        				appointmentsMap[sspTo] = appointments;
+                        				
+                        				// now check if we're done by comparing the number of keys to the number of
+                        				// practices
+                        				if(Object.keys(appointmentsMap).length == $scope.foundPractices.length) {
+                        					onFindAllAppointmentsDone(appointmentsMap);
+                        				}
+                        			}
+        						},
+        						function(findAllAppointmentsFailure) {
+        							var sspTo = findAllAppointmentsFailure.config.headers['Ssp-To'];
+                    				appointmentsMap[sspTo] = [];
+                    				
+                    				// now check if we're done by comparing the number of keys to the number of
+                    				// practices
+                    				if(Object.keys(appointmentsMap).length == $scope.foundPractices.length) {
+                    					onFindAllAppointmentsDone(appointmentsMap);
+                    				}                    				
+        						}
+            				);
             			}, 
             			function(getPatientFhirIdFailure) {		
             				var sspTo = getPatientFhirIdFailure.config.headers['Ssp-To'];
@@ -43,59 +75,22 @@ angular.module('gpConnect')
             			}
                 	);                	
             	}
-            	
-            	$q.all(appointmentsPromises).then(
-        			function(findAllAppointmentsSuccess) {
-                    	var allPracticeAppointments = [];
-                    	
-                    	for(var appointmentsIndex in findAllAppointmentsSuccess) {
-                			var appointmentsResponse = appointmentsResponses[appointmentsIndex];
-                			var appointments = appointmentsResponse.data.entry;
-                			
-                			if(appointments != undefined && appointments.length > 0) {
-                				setCancellationReason(appointments);
-                				allPracticeAppointments = allPracticeAppointments.concat(appointments);
-                			}
-                		}
-                		
-                    	// sort by appointment start datetime ascending
-                    	allPracticeAppointments.sort(function (a, b) {
-                            return a.resource.start.localeCompare(b.resource.start);
-                        });
-                    	
-                    	$scope.appointments = allPracticeAppointments;
-                    	usSpinnerService.stop('patientSummary-spinner');            				
-        			},
-        			function(findAllAppoitnmentFailure) {
-        				usSpinnerService.stop('patientSummary-spinner');   
-        			}
-            	);
             }
             
-            function onFindAllAppointmentsSuccess(appointmentsResponses) {
+            function onFindAllAppointmentsDone(appointmentsMap) {
             	var allPracticeAppointments = [];
             	
-            	for(var appointmentsIndex in appointmentsResponses) {
-        			var appointmentsResponse = appointmentsResponses[appointmentsIndex];
-        			var appointments = appointmentsResponse.data.entry;
-        			
-        			if(appointments != undefined && appointments.length > 0) {
-        				setCancellationReason(appointments);
-        				allPracticeAppointments = allPracticeAppointments.concat(appointments);
-        			}
-        		}
-        		
+            	for(var practice in appointmentsMap) {
+            		allPracticeAppointments = allPracticeAppointments.concat(appointmentsMap[practice]);
+            	}
+            	
             	// sort by appointment start datetime ascending
             	allPracticeAppointments.sort(function (a, b) {
                     return a.resource.start.localeCompare(b.resource.start);
                 });
             	
             	$scope.appointments = allPracticeAppointments;
-            	usSpinnerService.stop('patientSummary-spinner');        	
-            }
-            
-            function onFindAllAppointmentsFailure(appointmentsResponses) {
-            	alert("Shit went bad");
+            	usSpinnerService.stop('patientSummary-spinner');   
             }
             
             function setCancellationReason(practiceAppointments) {

d3e71e08c8148e8df7181292e5ef8d58f1304c2a
Chris Jobling <christopher.jobling@answerdigital.com>
2016-12-15 16:50:58 +0000

    GPC- 210, Added Error handling for getCareRecord, getschedule and registerPatient

--- gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/organization/GetScheduleOperation.java
+++ gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/organization/GetScheduleOperation.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ca.uhn.fhir.model.api.ExtensionDt;
+import ca.uhn.fhir.model.dstu2.composite.CodeableConceptDt;
+import ca.uhn.fhir.model.dstu2.composite.CodingDt;
 import ca.uhn.fhir.model.dstu2.composite.IdentifierDt;
 import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
 import ca.uhn.fhir.model.dstu2.resource.Bundle;
@@ -17,8 +19,10 @@ import ca.uhn.fhir.model.dstu2.resource.Practitioner;
 import ca.uhn.fhir.model.dstu2.resource.Schedule;
 import ca.uhn.fhir.model.dstu2.resource.Slot;
 import ca.uhn.fhir.model.dstu2.valueset.IssueSeverityEnum;
+import ca.uhn.fhir.model.dstu2.valueset.IssueTypeEnum;
 import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.rest.param.TokenParam;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import uk.gov.hscic.appointments.ScheduleResourceProvider;
 import uk.gov.hscic.appointments.SlotResourceProvider;
 import uk.gov.hscic.location.LocationResourceProvider;
@@ -31,112 +35,143 @@ import uk.gov.hscic.practitioner.PractitionerResourceProvider;
 @Component
 public class GetScheduleOperation {
 
-    @Autowired
-    LocationResourceProvider locationResourceProvider;
+	@Autowired
+	LocationResourceProvider locationResourceProvider;
 
-    @Autowired
-    PractitionerResourceProvider practitionerResourceProvider;
+	@Autowired
+	PractitionerResourceProvider practitionerResourceProvider;
 
-    @Autowired
-    SlotResourceProvider slotResourceProvider;
+	@Autowired
+	SlotResourceProvider slotResourceProvider;
 
-    @Autowired
-    OrganizationResourceProvider organizationResourceProvider;
+	@Autowired
+	OrganizationResourceProvider organizationResourceProvider;
 
-    @Autowired
-    ScheduleResourceProvider scheduleResourceProvider;
+	@Autowired
+	ScheduleResourceProvider scheduleResourceProvider;
 
-    @SuppressWarnings("deprecation")
-    void populateBundle(Bundle bundle, OperationOutcome operationOutcome, IdDt orgId, String planningHorizonStart, String planningHorizonEnd) {
-        // organisation
-        Organization organization = organizationResourceProvider.getOrganizationById(orgId);
-        Entry organisationEntry = new Entry();
-        organisationEntry.setResource(organization);
-        organisationEntry.setFullUrl("Organisation/" + organization.getId().getIdPart());
-        bundle.addEntry(organisationEntry);
+	@SuppressWarnings("deprecation")
+	void populateBundle(Bundle bundle, OperationOutcome operationOutcome, IdDt orgId, String planningHorizonStart,
+			String planningHorizonEnd) {
+		// organisation
+		Organization organization = organizationResourceProvider.getOrganizationById(orgId);
+		if (organization == null) {
+			CodingDt errorCoding = new CodingDt()
+					.setSystem("http://fhir.nhs.net/ValueSet/gpconnect-error-or-warning-code-1")
+					.setCode("REFERENCE_NOT_FOUND");
+			CodeableConceptDt errorCodableConcept = new CodeableConceptDt().addCoding(errorCoding);
+			errorCodableConcept.setText("Invalid Reference");
+			operationOutcome.addIssue().setSeverity(IssueSeverityEnum.ERROR).setCode(IssueTypeEnum.NOT_FOUND)
+					.setDetails(errorCodableConcept);
+			throw new ResourceNotFoundException("organizationResource returning null");
+		}
 
-        // location
-        String organizationSiteOdsCode = null;
-        for (IdentifierDt identifier : organization.getIdentifier()) {
-            if ("http://fhir.nhs.net/Id/ods-site-code".equalsIgnoreCase(identifier.getSystem())) {
-                organizationSiteOdsCode = identifier.getValue();
-                break;
-            }
-        }
-        if (organizationSiteOdsCode != null) {
-            List<Location> locations = locationResourceProvider.getByIdentifierCode(new TokenParam("http://fhir.nhs.net/Id/ods-site-code", organizationSiteOdsCode));
-            Entry locationEntry = new Entry();
-            locationEntry.setResource(locations.get(0));
-            locationEntry.setFullUrl("Location/" + locations.get(0).getId().getIdPart());
-            bundle.addEntry(locationEntry);
+		Entry organisationEntry = new Entry();
+		organisationEntry.setResource(organization);
+		organisationEntry.setFullUrl("Organisation/" + organization.getId().getIdPart());
+		bundle.addEntry(organisationEntry);
 
-            // schedules
-            List<Schedule> schedules = scheduleResourceProvider.getSchedulesForLocationId(locations.get(0).getId().getIdPart(), planningHorizonStart, planningHorizonEnd);
-            if (schedules.isEmpty() == false) {
-                for (Schedule schedule : schedules) {
-                    Entry scheduleEntry = new Entry();
-                    scheduleEntry.setResource(schedule);
-                    scheduleEntry.setFullUrl("Schedule/" + schedule.getId().getIdPart());
-                    bundle.addEntry(scheduleEntry);
+		// location
+		String organizationSiteOdsCode = null;
+		for (IdentifierDt identifier : organization.getIdentifier()) {
+			if ("http://fhir.nhs.net/Id/ods-site-code".equalsIgnoreCase(identifier.getSystem())) {
+				organizationSiteOdsCode = identifier.getValue();
+				break;
+			}
+		}
+		if (organizationSiteOdsCode != null) {
+			List<Location> locations = locationResourceProvider.getByIdentifierCode(
+					new TokenParam("http://fhir.nhs.net/Id/ods-site-code", organizationSiteOdsCode));
+			Entry locationEntry = new Entry();
+			locationEntry.setResource(locations.get(0));
+			locationEntry.setFullUrl("Location/" + locations.get(0).getId().getIdPart());
+			bundle.addEntry(locationEntry);
 
-                    // practitioners
-                    List<ExtensionDt> practitionerExtensions = scheduleResourceProvider.getPractitionerReferences(schedule);
-                    if (practitionerExtensions.isEmpty() == false) {
-                        for (ExtensionDt practionerExtension : practitionerExtensions) {
-                            ResourceReferenceDt practitionerRef = (ResourceReferenceDt) practionerExtension.getValue();
-                            Practitioner practitioner = practitionerResourceProvider.getPractitionerById(practitionerRef.getReferenceElement());
+			// schedules
+			List<Schedule> schedules = scheduleResourceProvider.getSchedulesForLocationId(
+					locations.get(0).getId().getIdPart(), planningHorizonStart, planningHorizonEnd);
+			if (schedules.isEmpty() == false) {
+				for (Schedule schedule : schedules) {
+					Entry scheduleEntry = new Entry();
+					scheduleEntry.setResource(schedule);
+					scheduleEntry.setFullUrl("Schedule/" + schedule.getId().getIdPart());
+					bundle.addEntry(scheduleEntry);
 
-                            Entry practionerEntry = new Entry();
-                            practionerEntry.setResource(practitioner);
-                            practionerEntry.setFullUrl("Practitioner/" + practitioner.getId().getIdPart());
-                            bundle.addEntry(practionerEntry);
-                        }
-                    } else {
-                        String msg = String.format("No practitioners could be found for the schedule %s", schedule.getId().getIdPart());
-                        operationOutcome.addIssue().setSeverity(IssueSeverityEnum.INFORMATION).setDetails(msg);
+					// practitioners
+					List<ExtensionDt> practitionerExtensions = scheduleResourceProvider
+							.getPractitionerReferences(schedule);
+					if (practitionerExtensions.isEmpty() == false) {
+						for (ExtensionDt practionerExtension : practitionerExtensions) {
+							ResourceReferenceDt practitionerRef = (ResourceReferenceDt) practionerExtension.getValue();
+							Practitioner practitioner = practitionerResourceProvider
+									.getPractitionerById(practitionerRef.getReferenceElement());
+							if (practitioner == null) {
+								CodingDt errorCoding = new CodingDt()
+										.setSystem("http://fhir.nhs.net/ValueSet/gpconnect-error-or-warning-code-1")
+										.setCode("REFERENCE_NOT_FOUND");
+								CodeableConceptDt errorCodableConcept = new CodeableConceptDt().addCoding(errorCoding);
+								errorCodableConcept.setText("Invalid Reference");
+								operationOutcome.addIssue().setSeverity(IssueSeverityEnum.ERROR)
+										.setCode(IssueTypeEnum.NOT_FOUND).setDetails(errorCodableConcept);
+								throw new ResourceNotFoundException("Practitioner Reference returning null");
+							}
+							Entry practionerEntry = new Entry();
+							practionerEntry.setResource(practitioner);
+							practionerEntry.setFullUrl("Practitioner/" + practitioner.getId().getIdPart());
+							bundle.addEntry(practionerEntry);
+						}
+					} else {
+						String msg = String.format("No practitioners could be found for the schedule %s",
+								schedule.getId().getIdPart());
+						operationOutcome.addIssue().setSeverity(IssueSeverityEnum.INFORMATION).setDetails(msg);
 
-                        Entry operationOutcomeEntry = new Entry();
-                        operationOutcomeEntry.setResource(operationOutcome);
-                        bundle.addEntry(operationOutcomeEntry);
-                    }
+						Entry operationOutcomeEntry = new Entry();
+						operationOutcomeEntry.setResource(operationOutcome);
+						bundle.addEntry(operationOutcomeEntry);
+					}
 
-                    // slots
-                    List<Slot> slots = slotResourceProvider.getSlotsForScheduleId(schedule.getId().getIdPart(), planningHorizonStart, planningHorizonEnd);
-                    if (slots.isEmpty() == false) {
-                        for (Slot slot : slots) {
-                            if ("FREE".equalsIgnoreCase(slot.getFreeBusyType())) {
-                                Entry slotEntry = new Entry();
-                                slotEntry.setResource(slot);
-                                slotEntry.setFullUrl("Slot/" + slot.getId().getIdPart());
-                                bundle.addEntry(slotEntry);
-                            }
-                        }
-                    } else {
-                        String msg = String.format("No slots could be found for the schedule %s within the specified planning horizon (start - %s end - %s)", schedule.getId().getIdPart(), planningHorizonStart, planningHorizonEnd);
-                        operationOutcome.addIssue().setSeverity(IssueSeverityEnum.INFORMATION).setDetails(msg);
+					// slots
+					List<Slot> slots = slotResourceProvider.getSlotsForScheduleId(schedule.getId().getIdPart(),
+							planningHorizonStart, planningHorizonEnd);
+					if (slots.isEmpty() == false) {
+						for (Slot slot : slots) {
+							if ("FREE".equalsIgnoreCase(slot.getFreeBusyType())) {
+								Entry slotEntry = new Entry();
+								slotEntry.setResource(slot);
+								slotEntry.setFullUrl("Slot/" + slot.getId().getIdPart());
+								bundle.addEntry(slotEntry);
+							}
+						}
+					} else {
+						String msg = String.format(
+								"No slots could be found for the schedule %s within the specified planning horizon (start - %s end - %s)",
+								schedule.getId().getIdPart(), planningHorizonStart, planningHorizonEnd);
+						operationOutcome.addIssue().setSeverity(IssueSeverityEnum.INFORMATION).setDetails(msg);
 
-                        Entry operationOutcomeEntry = new Entry();
-                        operationOutcomeEntry.setResource(operationOutcome);
-                        bundle.addEntry(operationOutcomeEntry);
-                    }
+						Entry operationOutcomeEntry = new Entry();
+						operationOutcomeEntry.setResource(operationOutcome);
+						bundle.addEntry(operationOutcomeEntry);
+					}
 
-                }
-            } else {
-                String msg = String.format("No schedules could be found for the location within the planning horizon (start - %s end - %s)", planningHorizonStart, planningHorizonEnd);
-                operationOutcome.addIssue().setSeverity(IssueSeverityEnum.INFORMATION).setDetails(msg);
+				}
+			} else {
+				String msg = String.format(
+						"No schedules could be found for the location within the planning horizon (start - %s end - %s)",
+						planningHorizonStart, planningHorizonEnd);
+				operationOutcome.addIssue().setSeverity(IssueSeverityEnum.INFORMATION).setDetails(msg);
 
-                Entry operationOutcomeEntry = new Entry();
-                operationOutcomeEntry.setResource(operationOutcome);
-                bundle.addEntry(operationOutcomeEntry);
-            }
-        } else {
-            String msg = String.format("No organisation could be found for the organisation ID %s", orgId);
-            operationOutcome.addIssue().setSeverity(IssueSeverityEnum.INFORMATION).setDetails(msg);
-            Entry operationOutcomeEntry = new Entry();
-            operationOutcomeEntry.setResource(operationOutcome);
-            bundle.addEntry(operationOutcomeEntry);
-        }
+				Entry operationOutcomeEntry = new Entry();
+				operationOutcomeEntry.setResource(operationOutcome);
+				bundle.addEntry(operationOutcomeEntry);
+			}
+		} else {
+			String msg = String.format("No organisation could be found for the organisation ID %s", orgId);
+			operationOutcome.addIssue().setSeverity(IssueSeverityEnum.INFORMATION).setDetails(msg);
+			Entry operationOutcomeEntry = new Entry();
+			operationOutcomeEntry.setResource(operationOutcome);
+			bundle.addEntry(operationOutcomeEntry);
+		}
 
-    }
+	}
 
 }

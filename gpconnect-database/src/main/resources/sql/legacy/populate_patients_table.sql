cb1e50d70c35595f1dc3c5ca268f957d49e7d769
Chris Jobling <christopher.jobling@answerdigital.com>
2017-01-05 15:09:59 +0000

    Fixed issue with the new test patients

--- gpconnect-database/src/main/resources/sql/legacy/populate_patients_table.sql
+++ gpconnect-database/src/main/resources/sql/legacy/populate_patients_table.sql
@@ -1,12 +1,10 @@
 LOCK TABLES gpconnect.patients WRITE;
 INSERT INTO gpconnect.patients
-  (id,    title,   first_name,   last_name,    address_1, address_2, address_3, address_4, address_5, postcode,   phone, date_of_birth, gender, nhs_number, pas_number, department_id, gp_id, lastUpdated,sensitive_flag,primary_care_code,date_of_death)
+  (id,    title,   first_name,   last_name,    address_1,address_2,address_3,postcode,phone,date_of_birth, gender,nhs_number,pas_number,department_id, gp_id, lastUpdated)
 VALUE
-   (1,'MRS','Minnie','DAWES','','24 GRAMMAR SCHOOL ROAD','','BRIGG','','DN20 8AF','',31/05/1952,'',9476719931,352541,1,3,21/01/2011,'','A21472',null),
-   (2,'MRS','Dolly','BANTON','','11 QUEENSWAY','','SCUNTHORPE','','DN16 2BZ','',31/05/1952,'',9476719931,352541,1,3,21/01/2011,'','A21472',null),
-   (3,'MRS','Minnie','DAWES','','24 GRAMMAR SCHOOL ROAD','','BRIGG','','DN20 8AF','',31/05/1952,'',9476719931,352541,1,3,21/01/2011,'','A21472',null),
-   (4,'MRS','Minnie','DAWES','','24 GRAMMAR SCHOOL ROAD','','BRIGG','','DN20 8AF','',31/05/1952,'',9476719931,352541,1,3,21/01/2011,'','A21472',null);
+   (1,'MRS','Minnie','DAWES','24 GRAMMAR SCHOOL ROAD','BRIGG','','DN20 8AF','01454587554','1952-05-31','Female',9476719931,352541,1,3,'2016-07-25 12:00:00'),
+   (2,'MRS','Dolly','BANTON','11 QUEENSWAY','SCUNTHORPE','S HUMBERSIDE','DN16 2BZ','0121454552','1955-09-18','Female',9476719958,623454,1,2,'2016-07-25 12:00:00'),
+   (3,'MISS','Ruby','MACKIN','3 WILDERSPIN HEIGHTS','BARTON-UPON-HUMBER','S HUMBERSIDE','DN18 5SN','013256541523','1953-01-01','Female',9476719966,346574,2,1,'2016-07-25 12:00:00'),
+   (4,'MR','Julian','PHELAN','FARM HOUSE','BONNYHALE ROAD','S HUMBERSIDE','DN17 4JQ','02123636563','1992-07-02','Male',9476719974,332546,1,3,'2016-07-25 12:00:00');
 
 UNLOCK TABLES;
-
-	

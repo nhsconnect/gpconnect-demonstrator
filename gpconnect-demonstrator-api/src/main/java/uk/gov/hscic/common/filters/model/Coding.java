b7beae7cb84296f9d2ea34c89e6c0dd5f6312cb1
Kris Bloe <kris.bloe@answerdigital.com>
2017-02-01 12:06:04 +0000

    Validate Practitioner Role.

---
+++ gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/common/filters/model/Coding.java
@@ -0,0 +1,22 @@
+package uk.gov.hscic.common.filters.model;
+
+public class Coding {
+    private String system;
+    private String code;
+
+    public String getSystem() {
+        return system;
+    }
+
+    public void setSystem(String system) {
+        this.system = system;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
--- gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/common/filters/model/RequestingPractitioner.java
+++ gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/common/filters/model/RequestingPractitioner.java
@@ -2,6 +2,7 @@ package uk.gov.hscic.common.filters.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +11,9 @@ public class RequestingPractitioner {
     private String id;
     private String resourceType;
     private Map<String, List<String>> name;
+
+    @JsonProperty("practitionerRole")
+    private List<Map<String, Map<String, List<Coding>>>> practitionerRoles;
 
     @JsonProperty("identifier")
     private List<Identifier> identifiers;
@@ -49,5 +53,25 @@ public class RequestingPractitioner {
                 .map(Identifier::getValue)
                 .findFirst()
                 .orElse(null);
+    }
+
+    public String getPractitionerRoleCode(String system) {
+        if (practitionerRoles.isEmpty()) {
+            return null;
+        }
+
+        return practitionerRoles
+                .get(0)
+                .getOrDefault("role", Collections.emptyMap())
+                .getOrDefault("coding", Collections.emptyList())
+                .stream()
+                .filter(coding -> system.equals(coding.getSystem()))
+                .map(Coding::getCode)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void setPractitionerRoles(List<Map<String, Map<String, List<Coding>>>> practitionerRoles) {
+        this.practitionerRoles = practitionerRoles;
     }
 }

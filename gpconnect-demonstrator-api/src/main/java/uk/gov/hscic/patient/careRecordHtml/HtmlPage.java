97c766ccba3467ffbb29da06a7c7619c0052d913
james-answer <james.cox@answerdigital.com>
2017-02-27 15:18:47 +0000

    Updated the PRB section HTML builder to new builder

---
+++ gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/patient/careRecordHtml/HtmlPage.java
@@ -0,0 +1,39 @@
+package uk.gov.hscic.patient.careRecordHtml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HtmlPage {
+
+    private final String header;
+    private final String name;
+    private final String code;
+    private final List<PageSection> pageSections;
+    
+    public HtmlPage(String pageHeader, String pageName, String pageCode) {
+        header = pageHeader;
+        name = pageName;
+        code = pageCode;
+        pageSections = new ArrayList<>();
+    }
+    
+    public void addPageSection(PageSection section){
+        pageSections.add(section);
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getCode() {
+        return code;
+    }
+
+    public List<PageSection> getPageSections() {
+        return pageSections;
+    }
+}

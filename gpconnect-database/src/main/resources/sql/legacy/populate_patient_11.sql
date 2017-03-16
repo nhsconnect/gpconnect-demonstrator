6d3bb0d9b4e2bf00b46bf8164e4a8a7fa2e7a6dc
Kris Bloe <kris.bloe@answerdigital.com>
2017-03-16 14:53:49 +0000

    Setting actual patient data in SQL.

---
+++ gpconnect-database/src/main/resources/sql/legacy/populate_patient_11.sql
@@ -0,0 +1,48 @@
+INSERT INTO gpconnect.adminitems
+  (nhsNumber,sectionDate,adminDate,Entry,Details)
+VALUES
+  (9000000114,'2015-05-01 12:17:00','2015-05-01 12:17:00',"Cervical Smear Defaulter","");
+
+INSERT INTO gpconnect.allergies
+  (nhsNumber,currentOrHistoric,startDate,endDate,details)
+VALUES
+  (9000000114,"Historical",'2015-05-01 12:17:00','2015-05-01 12:17:00',"Skin allergy"),
+  (9000000114,"Current",'2015-05-01 12:17:00','2015-05-01 12:17:00',"Work allergy");
+
+INSERT INTO gpconnect.clinicalitems
+  (nhsNumber,sectionDate,dateOfItem,Entry,Details)
+VALUES
+  (9000000114,'2015-05-01 12:17:00','2015-05-01 12:17:00',"Abdominal X-ray","No evidence of osteomyelitis.");
+
+INSERT INTO gpconnect.encounters
+  (nhsNumber,sectionDate,encounterDate,title,details)
+VALUES
+  (9000000114,'2016-07-01 12:17:00','2016-07-01 12:17:00',"Miss Tanya Turnpike (Practice Nurse) - Dr Johnson and Partners (J12345)","Result : Full blood count - FBC - Normal - No Action.");
+
+INSERT INTO gpconnect.immunisations
+  (nhsNumber,dateOfVac,vaccination,part,contents,details)
+VALUES
+  (9000000114,'2016-07-01 09:22:00',"HIV Injection","Manufacturer : fred<br />Batch: 1<br />Injection Location:Left leg<br />Expiry Date: 04-Oct-2016","Dont know","Dont Know");
+
+INSERT INTO gpconnect.medications_html
+  (nhsNumber,currentRepeatPast,startDate,medicationItem,scheduledEnd,daysDuration,details,lastIssued,reviewDate,numberIssued,maxIssues,typeMed)
+VALUES
+  (9000000114,"Current","01-07-2016","Amoxicillin 500mg capsules Supply (42) capsule(s)","19-11-2019","Day Duration 4","3",NULL,NULL,NULL,NULL,"2"),
+  (9000000114,"Repeat","01-07-2016","Metformin 500mg tablets","19-11-2019","Day Duration 4","3","Issue more","Issue more","Issue more","Issue more","4"),
+  (9000000114,"Past","01-07-2016","Metformin 500mg tablets","19-11-2019","Day Duration 4","3","Issue more","Issue more","Issue more","Issue more","4");
+
+INSERT INTO gpconnect.observations
+  (nhsNumber,observationDate,entry,value,details)
+VALUES
+  (9000000114,'2016-07-01 12:00:00',"Health of Nat Outc Sc item 1 - aggressive/disrupt behaviour","0","(Added from Questionnaire)");
+
+INSERT INTO gpconnect.problems
+  (nhsNumber,activeOrInactive,startDate,endDate,entry,significance,details)
+VALUES
+  (9000000114,"Active",'2016-07-01 12:00:00','2016-07-01 12:00:00',"Low Back Pain","Minor","Treated daily"),
+  (9000000114,"Inactive",'2016-07-01 12:00:00','2016-07-01 12:00:00',"Asthma","Major","Treated daily");
+
+INSERT INTO gpconnect.referrals
+  (nhsNumber,sectionDate,referral_from,referral_to,priority,details,lastUpdated)
+VALUES
+  (9000000114,'2016-07-01 00:00:01','Dr Johnson &amp; Partners','Leeds District Nurses','Routine','Referral to local authority weight management programme<br />Waiting For Information','2016-07-25 12:00:00');
---
+++ gpconnect-database/src/main/resources/sql/legacy/populate_patient_5.sql
@@ -0,0 +1,4 @@
+INSERT INTO gpconnect.medications_html
+  (nhsNumber,currentRepeatPast,startDate,medicationItem,scheduledEnd,daysDuration,details,lastIssued,reviewDate,numberIssued,maxIssues,typeMed)
+VALUES
+  (9476719966,"Past","01-07-2016","Metformin 500mg tablets","19-11-2019","Day Duration 4","3","Issue more","Issue more","Issue more","Issue more","4");

cc18ac7b7953928bd96fae35d38a0ef23183a87b
wweatherill-answer <will.weatherill@answerconsulting.com>
2017-05-16 10:42:57 +0100

    Fixing problem with interaction validation which was being too strict.

--- gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/common/filters/FhirRequestGenericIntercepter.java
+++ gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/common/filters/FhirRequestGenericIntercepter.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletException;
@@ -36,46 +34,16 @@ import uk.gov.hscic.SystemHeader;
 import uk.gov.hscic.SystemParameter;
 import uk.gov.hscic.SystemURL;
 import uk.gov.hscic.auth.CertificateValidator;
+import uk.gov.hscic.common.filters.model.Interactions;
+import uk.gov.hscic.common.filters.model.Interactions.Interaction;
 import uk.gov.hscic.common.ldap.model.ProviderRouting;
 
 @Component
 public class FhirRequestGenericIntercepter extends InterceptorAdapter {
     private static final Logger LOG = Logger.getLogger(FhirRequestGenericIntercepter.class);
-
-    private static final Map<String, String> INTERACTION_MAP = new HashMap<String, String>() {{
-        put(InteractionId.CLAIM_PATIENT_ALLERGY_INTOLERANCE, "/fhir/AllergyIntolerance");
-        put(InteractionId.CLAIM_PATIENT_APPOINTMENT, "/fhir/Appointment");
-        put(InteractionId.CLAIM_PATIENT_CONDITION, "/fhir/Condition");
-        put(InteractionId.CLAIM_PATIENT_DIAGNOSTIC_ORDER, "/fhir/DiagnosticOrder");
-        put(InteractionId.CLAIM_PATIENT_DIAGNOSTIC_REPORT, "/fhir/DiagnosticReport");
-        put(InteractionId.CLAIM_PATIENT_ENCOUNTER, "/fhir/Encounter");
-        put(InteractionId.CLAIM_PATIENT_FLAG, "/fhir/Flag");
-        put(InteractionId.CLAIM_PATIENT_IMMUNIZATION, "/fhir/Immunization");
-        put(InteractionId.CLAIM_PATIENT_MEDICATION_ADMINISTRATION, "/fhir/MedicationAdministration");
-        put(InteractionId.CLAIM_PATIENT_MEDICATION_DISPENSE, "/fhir/MedicationDispense");
-        put(InteractionId.CLAIM_PATIENT_MEDICATION_ORDER, "/fhir/MedicationOrder");
-        put(InteractionId.CLAIM_PATIENT_OBSERVATION, "/fhir/Observation");
-        put(InteractionId.CLAIM_PATIENT_PROBLEM, "/fhir/Problem");
-        put(InteractionId.CLAIM_PATIENT_PROCEDURES, "/fhir/Procedure");
-        put(InteractionId.CLAIM_PATIENT_REFERRAL, "/fhir/Referral");
-        put(InteractionId.OPERATION_GPC_GET_CARE_RECORD, "/fhir/Patient/$gpc.getcarerecord");
-        put(InteractionId.OPERATION_GPC_GET_SCHEDULE, "/fhir/Organization/%ID%/$gpc.getschedule");
-        put(InteractionId.OPERATION_GPC_REGISTER_PATIENT, "/fhir/Patient/$gpc.registerpatient");
-        put(InteractionId.REST_CREATE_APPOINTMENT, "/fhir/Appointment");
-        put(InteractionId.REST_CREATE_ORDER, "/fhir/Order");
-        put(InteractionId.REST_READ_APPOINTMENT, "/fhir/Appointment/%ID%");
-        put(InteractionId.REST_READ_LOCATION, "/fhir/Location/%ID%");
-        put(InteractionId.REST_READ_METADATA, "/fhir/metadata");
-        put(InteractionId.REST_READ_ORGANIZATION, "/fhir/Organization/%ID%");
-        put(InteractionId.REST_READ_PATIENT, "/fhir/Patient/%ID%");
-        put(InteractionId.REST_READ_PRACTITIONER, "/fhir/Practitioner/%ID%");
-        put(InteractionId.REST_SEARCH_LOCATION, "/fhir/Location");
-        put(InteractionId.REST_SEARCH_ORGANIZATION, "/fhir/Organization");
-        put(InteractionId.REST_SEARCH_PATIENT, "/fhir/Patient");
-        put(InteractionId.REST_SEARCH_PATIENT_APPOINTMENTS, "/fhir/Patient/%ID%/Appointment");
-        put(InteractionId.REST_SEARCH_PRACTITIONER, "/fhir/Practitioner");
-        put(InteractionId.REST_UPDATE_APPOINTMENT, "/fhir/Appointment/%ID%");
-    }};
+    
+    @Autowired
+    private Interactions interactions;
 
     @Autowired
     private CertificateValidator certificateValidator;
@@ -133,11 +101,9 @@ public class FhirRequestGenericIntercepter extends InterceptorAdapter {
             throwInvalidRequestException(SystemHeader.SSP_INTERACTIONID + " header blank");
         }
 
-        String url = httpRequest.getRequestURI();
-        if (!url.equals(getInteractionAndAddId(interactionIdHeader, url))) {
-            throwBadRequestException("InteractionId Incorrect");
-        }
-
+        String requestURI = httpRequest.getRequestURI();
+        validateURIAgainstInteraction(interactionIdHeader, requestURI);
+        
         if (InteractionId.IDENTIFIER_INTERACTIONS.contains(interactionIdHeader)) {
             String[] identifiers = httpRequest.getParameterMap().get(SystemParameter.IDENTIFIER);
 
@@ -208,6 +174,31 @@ public class FhirRequestGenericIntercepter extends InterceptorAdapter {
                 new UnprocessableEntityException(exceptionMessage),
                 SystemCode.INVALID_PARAMETER, IssueTypeEnum.INVALID_CONTENT);
     }
+    
+    private static void throwResourceNotFoundException(String exceptionMessage, String resource) {
+		String systemCode = null;
+		
+		switch(resource) {
+            case "Organization":
+                systemCode = SystemCode.ORGANISATION_NOT_FOUND;
+                break;
+
+            case "Patient":
+                systemCode = SystemCode.PATIENT_NOT_FOUND;
+                break;
+
+            case "Practitioner":
+                systemCode = SystemCode.PRACTITIONER_NOT_FOUND;
+                break;
+
+            default:
+                systemCode = SystemCode.REFERENCE_NOT_FOUND;                
+		}
+    	
+		throw OperationOutcomeFactory.buildOperationOutcomeException(
+		new ResourceNotFoundException(exceptionMessage),
+		systemCode, IssueTypeEnum.INVALID_CONTENT);   	
+    }
 
     /**
      * Listens for any exceptions thrown. In the case of invalid parameters, we
@@ -275,209 +266,29 @@ public class FhirRequestGenericIntercepter extends InterceptorAdapter {
 
         return super.preProcessOutgoingException(theRequestDetails, theException, theServletRequest);
     }
-
-    // This method finds the ID and inserts it
-    // doesn't always insert the ID - as sometimes there isn't one to insert
-    private static String getInteractionAndAddId(String interactionIdHeader, String url) {
-    	String interactionAndId = null;
+    
+    private void validateURIAgainstInteraction(String interactionId, String requestURI) {
+    	Interaction interaction = interactions.getInteraction(interactionId);
     	
-    	Interaction interaction = Interaction.getInteraction(interactionIdHeader);
-    	if(interaction != null) {
-    		try {
-        		ResourcePath resourcePath = interaction.getResourcePath(url);
-        		
-        		if(resourcePath != null) {
-        			interactionAndId = interaction.merge(resourcePath);
-        		}
-        		else {
-        			// 400
-                    throw OperationOutcomeFactory.buildOperationOutcomeException(
-                            new InvalidRequestException(String.format("The given URL %s does not match the interaction scheme %s (interaction Id - %s)", url, interaction.toString(), interactionIdHeader)),
-                            SystemCode.BAD_REQUEST, IssueTypeEnum.INVALID_CONTENT);  
-        		}
-        	}
-        	catch(NumberFormatException nfe) {
-        		String systemCode = null;	
-
-        		switch(interaction.resourceType) {
-    	            case "Appointment":
-    	                systemCode = SystemCode.APPOINTMENT_NOT_FOUND;
-    	                break;
-    	
-    	            case "Location":
-    	                systemCode = SystemCode.LOCATION_NOT_FOUND;
-    	                break;
-    	
-    	            case "Organization":
-    	                systemCode = SystemCode.ORGANISATION_NOT_FOUND;
-    	                break;
-    	
-    	            case "Patient":
-    	                systemCode = SystemCode.PATIENT_NOT_FOUND;
-    	                break;
-    	                
-                    case "Practitioner":
-                        systemCode = SystemCode.PRACTITIONER_NOT_FOUND;
-                        break;	
-                        
-                    default:
-                        systemCode = SystemCode.REFERENCE_NOT_FOUND;                    
-        		}
-        		// 404
-                throw OperationOutcomeFactory.buildOperationOutcomeException(
-                        new ResourceNotFoundException("The resource identifier in " + url + " is not a number"),
-                        systemCode, IssueTypeEnum.INVALID_CONTENT);    		
-        	}
+    	if(interaction != null) { 	
+	    	if(interaction.validateResource(requestURI) == false) {
+				throwBadRequestException(String.format("Unknown resource in URI - %s", requestURI));  		
+	    	}
+	    	
+	    	if(interaction.validateIdentifier(requestURI) == false) {
+	    		throwResourceNotFoundException(String.format("Unknown resource identifier in URI - %s", requestURI), interaction.getResource());	
+	    	}
+	    	
+	    	if(interaction.validateContainedResource(requestURI) == false) {
+	    		throwBadRequestException(String.format("Unknown contained resource in URI - %s", requestURI));  			   		
+	    	}
+	    	
+	    	if(interaction.validateOperation(requestURI) == false) {
+	    		throwBadRequestException(String.format("Unknown resource operation in URI - %s", requestURI));    		
+	    	}
     	}
     	else {
-			// 400
-            throw OperationOutcomeFactory.buildOperationOutcomeException(
-                    new InvalidRequestException(String.format("The given URL %s does not match the interaction scheme matching the given ID (%s)", url, interactionIdHeader)),
-                    SystemCode.BAD_REQUEST, IssueTypeEnum.INVALID_CONTENT);  
+            throwBadRequestException(String.format("Unable to locate interaction corresponding to the given URI (%s)", requestURI));
     	}
-
-    	return interactionAndId;
-    }
-    
-    private static class Interaction {
-    	private String path = null;
-    	
-    	private String base = null;
-    	private String resourceType = null;
-    	private String resourceIdPlaceHolder = null;
-    	private String operation = null;
-    	
-		@Override
-    	public String toString() {
-    		return path;
-    	}
-    	
-    	private Interaction(String path) {
-    		this.path = path;
-    		
-    		String[] elements = getElements(path);
-    		
-    		// the path needs to have a minimum of 2 elements
-    		int nameCount = elements.length;
-			if(nameCount >= 3) {
-				base = getBase(elements);
-				resourceType = getResourceType(elements);
-    		
-	    		if(nameCount >= 4) {
-	    			resourceIdPlaceHolder = getResourceId(elements);
-	    			
-	    			if("%ID%".equals(resourceIdPlaceHolder) == false) {
-	    				throw new IllegalArgumentException("blah");
-	    			}
-	    			
-	    			if(nameCount >= 5) {
-	    				operation = getOperation(elements);
-	    			}    			
-	    		}
-    		}
-			else {
-				throw new IllegalArgumentException("blah");
-			}
-    	}
-    	
-    	private String getBase(String[] elements) {
-    		return getName(elements, 1);
-    	}
-    	
-    	private String getResourceType(String[] elements) {
-    		return getName(elements, 2);
-    	}
-    	
-    	private String getResourceId(String[] elements) {
-    		return getName(elements, 3);
-    	}
-    	
-    	private String getOperation(String[] elements) {
-    		return getName(elements, 4);
-    	}
-    	
-    	private String getName(String[] elements, int index) {
-    		String name = null;
-    		
-    		if((elements.length - 1) >= index) {
-    			name = elements[index];
-    		}
-    		
-    		return name;
-    	}
-    	
-    	private String merge(ResourcePath resourcePath) {
-    		String mergeResult = path;
-    		
-    		if(resourceIdPlaceHolder != null) {
-    			mergeResult = path.replace("%ID%", resourcePath.resourceId.toString());  			
-    		}
-
-    		return mergeResult; 
-    	}
-    	
-    	private ResourcePath getResourcePath(String path) {
-    		ResourcePath resourcePath = null;
-    		
-    		// check it matches this interaction
-    		String[] elements = getElements(path);
-
-    		boolean valid = false;
-    		if(base.equals(getBase(elements))) {
-        		if(resourceType.equals(getResourceType(elements))) {
-               		if(operation != null) {
-	        			if(operation.equals(getOperation(elements))) {
-	            			valid = true;
-	            		}
-        			}
-               		else {
-               			valid = true;
-               		}
-        		}
-    		}
-    		
-    		if(valid) {
-    			resourcePath = new ResourcePath(getBase(elements), getResourceType(elements), getResourceId(elements), getOperation(elements));
-    		}
-    		
-    		return resourcePath;
-    	}
-    	
-    	private String[] getElements(String path) {
-    		return path.split("/");
-    	}
-
-    	private static Interaction getInteraction(String interactionId) {
-    		Interaction interaction = null;
-    		
-    		String interactionString = INTERACTION_MAP.getOrDefault(interactionId, "INVALID");
-    		if("INVALID".equals(interactionString) == false) {
-    			try {
-    				interaction = new Interaction(interactionString);
-    			}
-    			catch(IllegalArgumentException iae) {
-    				interaction = null;
-    			}
-    		}
-    		
-    		return interaction;
-    	}
-    }
-    
-    private static class ResourcePath {
- 	
-    	private String base = null;
-    	private String resourceType = null;
-    	private Integer resourceId = null;
-    	private String operation = null;
-
-    	private ResourcePath(String base, String resourceType, String resourceId, String operation) {
-			this.base = base;
-			this.resourceType = resourceType;
-			if(resourceId != null) {
-				this.resourceId = Integer.parseInt(resourceId);
-			}
-			this.operation = operation;
-		}
-    }
+    }    
 }
---
+++ gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/common/filters/model/Interactions.java
@@ -0,0 +1,132 @@
+package uk.gov.hscic.common.filters.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+import uk.gov.hscic.InteractionId;
+
+@Component
+public class Interactions {
+	
+	public static class Interaction  {
+		private static final Pattern WILDCARD = Pattern.compile(".+");
+	
+		private String resource = null;
+		private Pattern resourcePattern = null;
+		private Pattern containedResourcePattern = null;
+		private Pattern identifierPattern = null;
+		private Pattern operationPattern = null;
+		
+		private String currentRegex = null;
+		
+		private Interaction() {
+			currentRegex = "/fhir";
+			
+			resourcePattern = WILDCARD;
+			containedResourcePattern = WILDCARD;
+			identifierPattern = WILDCARD;
+			operationPattern = WILDCARD;
+		}
+				
+		private Interaction resource(String resource) {
+			this.resource = resource;
+
+			resourcePattern = buildPattern(resource);
+			
+			return this;
+		}
+		
+		private Interaction containedResource(String containedResource) {
+			containedResourcePattern = buildPattern(containedResource);
+			
+			return this;
+		}
+		
+		private Interaction operation(String operation) {
+			this.operationPattern = buildPattern("\\" + operation);
+
+			return this;
+		}
+		
+		private Interaction identifier() {
+			this.identifierPattern = buildPattern("\\d+");
+			
+			return this;
+		}
+		
+		private Pattern buildPattern(String end) {
+			currentRegex = currentRegex + "/" + end;
+			
+			return Pattern.compile(currentRegex + ".*");
+		}
+
+		public String getResource() {
+			return resource;
+		}
+			
+		public boolean validateResource(String uri) {
+			return resourcePattern.matcher(uri).matches();
+		}
+		
+		public boolean validateContainedResource(String uri) {
+			return containedResourcePattern.matcher(uri).matches();
+		}
+		
+		public boolean validateIdentifier(String uri) {
+			return identifierPattern.matcher(uri).matches();
+		}
+		
+		public boolean validateOperation(String uri) {
+			return operationPattern.matcher(uri).matches();
+		}
+	}
+	
+	private Map<String, Interaction> interactions = null;
+	
+	@PostConstruct
+	public void postConstruct() {
+		interactions = new HashMap<String, Interaction>();
+		
+        interactions.put(InteractionId.CLAIM_PATIENT_ALLERGY_INTOLERANCE, new Interaction().resource("AllergyIntolerance"));
+        interactions.put(InteractionId.CLAIM_PATIENT_APPOINTMENT, new Interaction().resource("Appointment"));
+        interactions.put(InteractionId.CLAIM_PATIENT_CONDITION, new Interaction().resource("Condition"));
+        interactions.put(InteractionId.CLAIM_PATIENT_DIAGNOSTIC_ORDER, new Interaction().resource("DiagnosticOrder"));
+        interactions.put(InteractionId.CLAIM_PATIENT_DIAGNOSTIC_REPORT, new Interaction().resource("DiagnosticReport"));
+        interactions.put(InteractionId.CLAIM_PATIENT_ENCOUNTER, new Interaction().resource("Encounter"));
+        interactions.put(InteractionId.CLAIM_PATIENT_FLAG, new Interaction().resource("Flag"));
+        interactions.put(InteractionId.CLAIM_PATIENT_IMMUNIZATION, new Interaction().resource("Immunization"));
+        interactions.put(InteractionId.CLAIM_PATIENT_MEDICATION_ADMINISTRATION, new Interaction().resource("MedicationAdministration"));
+        interactions.put(InteractionId.CLAIM_PATIENT_MEDICATION_DISPENSE, new Interaction().resource("MedicationDispense"));
+        interactions.put(InteractionId.CLAIM_PATIENT_MEDICATION_ORDER, new Interaction().resource("MedicationOrder"));
+        interactions.put(InteractionId.CLAIM_PATIENT_OBSERVATION, new Interaction().resource("Observation"));
+        interactions.put(InteractionId.CLAIM_PATIENT_PROBLEM, new Interaction().resource("Problem"));
+        interactions.put(InteractionId.CLAIM_PATIENT_PROCEDURES, new Interaction().resource("Procedure"));
+        interactions.put(InteractionId.CLAIM_PATIENT_REFERRAL, new Interaction().resource("Referral"));
+        interactions.put(InteractionId.OPERATION_GPC_GET_CARE_RECORD, new Interaction().resource("Patient").operation("$gpc.getcarerecord"));
+        interactions.put(InteractionId.OPERATION_GPC_GET_SCHEDULE, new Interaction().resource("Organization").identifier().operation("$gpc.getschedule"));
+        interactions.put(InteractionId.OPERATION_GPC_REGISTER_PATIENT, new Interaction().resource("Patient").operation("$gpc.registerpatient"));
+        interactions.put(InteractionId.REST_CREATE_APPOINTMENT, new Interaction().resource("Appointment"));
+        interactions.put(InteractionId.REST_CREATE_ORDER, new Interaction().resource("Order"));
+        interactions.put(InteractionId.REST_READ_APPOINTMENT, new Interaction().resource("Appointment").identifier());
+        interactions.put(InteractionId.REST_READ_LOCATION, new Interaction().resource("Location").identifier());
+        interactions.put(InteractionId.REST_READ_METADATA, new Interaction().resource("metadata"));
+        interactions.put(InteractionId.REST_READ_ORGANIZATION, new Interaction().resource("Organization").identifier());
+        interactions.put(InteractionId.REST_READ_PATIENT, new Interaction().resource("Patient").identifier());
+        interactions.put(InteractionId.REST_READ_PRACTITIONER, new Interaction().resource("Practitioner").identifier());
+        interactions.put(InteractionId.REST_SEARCH_LOCATION, new Interaction().resource("Location"));
+        interactions.put(InteractionId.REST_SEARCH_ORGANIZATION, new Interaction().resource("Organization"));
+        interactions.put(InteractionId.REST_SEARCH_PATIENT, new Interaction().resource("Patient"));
+        interactions.put(InteractionId.REST_SEARCH_PATIENT_APPOINTMENTS, new Interaction().resource("Patient").identifier().containedResource("Appointment"));
+        interactions.put(InteractionId.REST_SEARCH_PRACTITIONER, new Interaction().resource("Practitioner"));
+        interactions.put(InteractionId.REST_UPDATE_APPOINTMENT, new Interaction().resource("Appointment").identifier());
+	}
+	
+	public Interaction getInteraction(String interactionId) {
+		return interactions.get(interactionId);
+	}
+}

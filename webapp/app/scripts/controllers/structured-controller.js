38d30d7dce0d376e35ef00e5ed858e97f16933f2
UPCO-DOMAIN\joblingc <christopher.jobling@answerdigital.com>
2018-03-26 17:00:32 +0100

    Removed unused data

--- webapp/app/scripts/controllers/structured-controller.js
+++ webapp/app/scripts/controllers/structured-controller.js
@@ -8,8 +8,6 @@ angular
         PatientService,
         $stateParams
     ) {
-        $scope.ActiveAllergies = {};
-        $scope.ResolvedAllergies = {};
         $scope.ActiveAllergiesList = [];
         $scope.ResolvedAllergiesList = [];
 

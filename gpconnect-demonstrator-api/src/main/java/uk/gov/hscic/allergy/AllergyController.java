31fb77d7a9b2952b800adb3aba1062f0d6e6feae
Saurabh Jain <saurabh.k.jain.1991@gmail.com>
2018-04-06 10:47:46 +0100

    Added a method to retrieve all the allergies for a patient

---
+++ gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/allergy/AllergyController.java
@@ -0,0 +1,26 @@
+package uk.gov.hscic.allergy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hscic.patient.allergies.AllergyEntity;
+import uk.gov.hscic.patient.allergies.AllergyRepository;
+
+import java.util.List;
+
+/**
+ * Created by jains on 06/04/2018.
+ */
+@RestController
+@RequestMapping("api/")
+public class AllergyController {
+    @Autowired
+    private AllergyRepository allergyRepository;
+
+    @GetMapping("/allergy/{nhsnumber}")
+    public List<String> getAllergiesByNhsNumber(@PathVariable("nhsnumber") String nhsnumber) {
+        return allergyRepository.findDistinctAllergiesByNhsNumber(nhsnumber);
+    }
+}

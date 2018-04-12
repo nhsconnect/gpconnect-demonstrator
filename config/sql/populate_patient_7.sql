f6b1c011e7d1f68d167fcf9735fca79d9f0d50ae
lucywilcock <lucy.wilcock@answerdigital.com>
2018-04-12 11:57:08 +0100

    Remove config/sql

--- config/sql/populate_patient_7.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO gpconnect.medications_html
-  (nhsNumber,currentRepeatPast,startDate,medicationItem,scheduledEnd,daysDuration,details,lastIssued,reviewDate,numberIssued,maxIssues,typeMed)
-VALUES
-  (9000000076,"Repeat","01-05-2015","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat"),
-  (9000000076,"Repeat","01-07-2016","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat"),
-  (9000000076,"Past","01-05-2015","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat"),
-  (9000000076,"Past","01-07-2016","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat");

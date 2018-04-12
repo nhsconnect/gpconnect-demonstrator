34a17c476ed4b8d983a036c8039363766fa9daa5
lucywilcock <lucy.wilcock@answerdigital.com>
2018-04-12 11:38:44 +0100

    Add new (semi-realistic) data

---
+++ config/sql/populate_patient_6.sql
@@ -0,0 +1,7 @@
+INSERT INTO gpconnect.medications_html
+  (nhsNumber,currentRepeatPast,startDate,medicationItem,scheduledEnd,daysDuration,details,lastIssued,reviewDate,numberIssued,maxIssues,typeMed)
+VALUES
+  (9000000068,"Current","01-05-2015","Lansoprazole 15mg gastro-resistant capsules","19-11-2019","2 /7","Take your doses 30 minutes before breakfast when your stomach is empty",NULL,NULL,NULL,NULL,"Acute"),
+  (9000000068,"Current","01-07-2016","Amoxicillin 500mg capsules Supply (42) capsule(s)","19-11-2019","7 /7","Take every 8 hours (three times a day) with or without food",NULL,NULL,NULL,NULL,"Acute"),
+  (9000000068,"Repeat","01-05-2015","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat"),
+  (9000000068,"Repeat","01-07-2016","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat");

94cf7b2f9de4759ce3a7e108e478bb04a0c8f8dc
lucywilcock <lucy.wilcock@answerdigital.com>
2018-04-27 14:24:31 +0100

    Add demonstrator warning banner

--- webapp/app/scripts/tenant/gpconnect/content.js
+++ webapp/app/scripts/tenant/gpconnect/content.js
@@ -2,5 +2,7 @@
 
 angular.module('gpConnect')
   .value("content", {
-    "gpconnect_title": "GP Connect 0.5.0 - Demonstrator"
+    "gpconnect_title": "GP Connect 0.5.0 - Demonstrator",
+    "gpconnect_privacy": "Warning! This is a test server. Do not store any information here that contains personal health information or any other confidential information. This server will be regularly purged and reloaded with fixed test data."
   });
+

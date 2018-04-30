96738435ebfdd500f9905754e334b7f6c8df25a0
James Cox <james.cox@answerdigital.com>
2018-04-30 08:14:15 +0100

    Changed code system for registration type

--- gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/SystemURL.java
+++ gpconnect-demonstrator-api/src/main/java/uk/gov/hscic/SystemURL.java
@@ -121,7 +121,7 @@ public final class SystemURL {
 
     // FHIR CodeSystem Constants
     public static final String CS_REGISTRATION_STATUS = "https://fhir.nhs.uk/CareConnect-RegistrationStatus-1";
-    public static final String CS_REGISTRATION_TYPE = "https://fhir.nhs.uk/STU3/ValueSet/CareConnect-RegistrationType-1";
+    public static final String CS_REGISTRATION_TYPE = "https://fhir.nhs.uk/STU3/CodeSystem/CareConnect-RegistrationType-1";
     public static final String CS_CC_ETHNIC_CATEGORY = "https://fhir.nhs.uk/CareConnect-EthnicCategory-1";
     public static final String CS_CC_RELIGIOUS_AFFILI = "https://fhir.nhs.uk/CareConnect-ReligiousAffiliation-1";
     public static final String CS_CC_RESIDENTIAL_STATUS = "https://fhir.nhs.uk/CareConnect-ResidentialStatus-1";

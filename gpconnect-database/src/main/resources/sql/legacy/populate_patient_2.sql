be2d602c55f5cf01bea3a8be3c7d1340cb19b42a
lucywilcock <lucy.wilcock@answerdigital.com>
2018-05-08 14:46:29 +0100

    More realistic patient data

--- gpconnect-database/src/main/resources/sql/legacy/populate_patient_2.sql
+++ gpconnect-database/src/main/resources/sql/legacy/populate_patient_2.sql
@@ -1,67 +1,60 @@
 INSERT INTO gpconnect.adminitems
   (nhsNumber,sectionDate,adminDate,Entry,Details)
 VALUES
-  (9476719931,'2015-05-01 12:17:00','2015-05-01 12:17:00',"Cervical Smear Defaulter",""),
-  (9476719931,'2016-07-01 12:17:00','2016-07-01 12:17:00',"No summary care record consent specified","No record");
+  (9476719931,'2015-04-22','2015-04-22',"Appointment Letter","Community diabetes clinic");
 
 INSERT INTO gpconnect.allergies
   (nhsNumber,currentOrHistoric,startDate,endDate,details)
 VALUES
-  (9476719931,"Historical",'2015-05-01 12:17:00','2015-05-01 12:17:00',"Skin allergy"),
-  (9476719931,"Historical",'2016-07-01 12:17:00','2016-07-01 12:17:00',"Beer allergy"),
-  (9476719931,"Current",'2015-05-01 12:17:00','2015-05-01 12:17:00',"Work allergy"),
-  (9476719931,"Current",'2016-07-01 12:17:00','2016-07-01 12:17:00',"Work allergy");
+  (9476719931,"Historic",'1963-06-02',null,"Hayfever, allergy to pollen"),
+  (9476719931,"Current",'2016-03-15',null,"Allergy to Penicillin, Patient experienced rash, nausea and vomiting");
 
 INSERT INTO gpconnect.clinicalitems
   (nhsNumber,sectionDate,dateOfItem,Entry,Details)
 VALUES
-  (9476719931,'2015-05-01 12:17:00','2015-05-01 12:17:00',"Abdominal X-ray","No evidence of osteomyelitis."),
-  (9476719931,'2016-07-01 12:17:00','2016-07-01 12:17:00',"Curvature of spine","");
+  (9476719931,'2001-06-20','2001-06-20',"Mammogram","Normal");
 
 INSERT INTO gpconnect.encounters
   (nhsNumber,sectionDate,encounterDate,title,details)
 VALUES
-  (9476719931,'2015-05-01 12:17:00','2015-05-01 12:17:00',"Dr Jeffrey Johnson - Dr Johnson and Partners (J12345)","Haemoglobin A1c level - IFCC standardised 20.5 mmol/mol.<br />Insulin isophane biphasic porcine 30/70 100units/ml suspension for injection 10ml vials"),
-  (9476719931,'2016-07-01 12:17:00','2016-07-01 12:17:00',"Miss Tanya Turnpike (Practice Nurse) - Dr Johnson and Partners (J12345)","Result : Full blood count - FBC - Normal - No Action.");
+  (9476719931,'2018-02-01','2018-02-01',"Care navigation","Reception signposting regarding exercise classes");
 
 INSERT INTO gpconnect.immunisations
   (nhsNumber,dateOfVac,vaccination,part,contents,details)
 VALUES
-  (9476719931,'2015-05-01 09:22:00',"PediaceExpiry Date: 04-Oct-2016","Dont know","DIPTHERIA, HIB, PERTUSSIS, POLIO, TETANUS","DIPTHERIA, HIB, PERTUSSIS, POLIO, TETANUS"),
-  (9476719931,'2016-07-01 09:22:00',"HIV Injection","Manufacturer : fred<br />Batch: 1<br />Injection Location:Left leg<br />Expiry Date: 04-Oct-2016","Dont know","Dont Know");
+  (9476719931,'2010-10-04',"Influenza vaccine","1","Influenza",""),
+  (9476719931,'2013-01-18',"Influenza vaccine","1","Influenza",""),
+  (9476719931,'2015-08-30',"Pneomovax II","1","Pneumoccocal","");
 
--- INSERT INTO gpconnect.investigations
---   (nhsNumber,sectionDate,title,details,lastUpdated)
--- VALUES
---   (9476719931,'2015-05-01 12:00:00','Long investigation',"Something wasn't found!",'2016-07-26 12:00:00'),
---   (9476719931,'2016-07-01 12:00:00','Long investigation',"Something wasn't found!",'2016-07-26 12:00:00');
+ INSERT INTO gpconnect.investigations
+   (nhsNumber,sectionDate,title,details,lastUpdated)
+ VALUES
+   (9476719931,'2018-04-26','Lumbar spine x-ray',"Normal result",'2018-04-26');
 
 INSERT INTO gpconnect.medications_html
   (nhsNumber,currentRepeatPast,startDate,medicationItem,scheduledEnd,daysDuration,details,lastIssued,reviewDate,numberIssued,maxIssues,typeMed)
 VALUES
-  (9476719931,"Current","01-05-2015","Lansoprazole 15mg gastro-resistant capsules","19-11-2019","2 /7","Take your doses 30 minutes before breakfast when your stomach is empty",NULL,NULL,NULL,NULL,"Acute"),
-  (9476719931,"Current","01-07-2016","Amoxicillin 500mg capsules Supply (42) capsule(s)","19-11-2019","7 /7","Take every 8 hours (three times a day) with or without food",NULL,NULL,NULL,NULL,"Acute"),
-  (9476719931,"Repeat","01-05-2015","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat"),
-  (9476719931,"Repeat","01-07-2016","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat"),
-  (9476719931,"Past","01-05-2015","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat"),
-  (9476719931,"Past","01-07-2016","Metformin 500mg tablets","19-11-2019","4 /7","Take with food and water","Issue more","Issue more","Issue more","Issue more","Repeat");
+  (9476719931,"Current","2018-04-25","Tramadol 50mg capsules","","28 days","Take 1 or 2 tablets as directed up to 4 times per day",NULL,NULL,NULL,NULL,"NHS Medication"),
+  (9476719931,"Repeat","2015-05-01","Metformin 500mg tablets","","","Take one tablet three times daily","2018-03-28","2018-10-15","3 issues","6 issues",""),
+  (9476719931,"Repeat","2006-03-01","Lercanidipine 20mg tablets","","","Take one tablet daily","2018-03-28","2018-10-15","3 issues","6 issues",""),
+  (9476719931,"Past","1978-09-03","Chloramphenicol eye drops preservative free 0.5%","","","1 drop in left eye four times daily for 4 weeks.",'','','','',"NHS Medication");
 
 INSERT INTO gpconnect.observations
   (nhsNumber,observationDate,entry,value,details)
 VALUES
-  (9476719931,'2015-05-01 12:00:00',"Full Health of the Nation Outcome Scale score","16","(Added from Questionnaire)"),
-  (9476719931,'2016-07-01 12:00:00',"Health of Nat Outc Sc item 1 - aggressive/disrupt behaviour","0","(Added from Questionnaire)");
+  (9476719931,'2017-05-07',"Foot Risk Classification","Left diabetic foot at low risk","Annual review"),
+  (9476719931,'2017-05-07',"Foot Risk Classification","Right diabetic foot at low risk","Annual review");
 
 INSERT INTO gpconnect.problems
   (nhsNumber,activeOrInactive,startDate,endDate,entry,significance,details)
 VALUES
-  (9476719931,"Active",'2015-05-01 12:00:00','2015-05-01 12:00:00',"Type II diabetes mellitus","Major","Treated daily"),
-  (9476719931,"Active",'2016-07-01 12:00:00','2016-07-01 12:00:00',"Low Back Pain","Minor","Treated daily"),
-  (9476719931,"Inactive",'2015-05-01 12:00:00','2015-05-01 12:00:00',"Type II diabetes mellitus","Major","Treated daily"),
-  (9476719931,"Inactive",'2016-07-01 12:00:00','2016-07-01 12:00:00',"Asthma","Major","Treated daily");
+  (9476719931,"Active",'2015-05-01',null,"Type II Diabetes mellitus","Major Active","Diabetes type II blood glucose management"),
+  (9476719931,"Active",'2006-03-01',null,"Essential hypertension","Major Active",""),
+  (9476719931,"Active",'2018-04-25',null,"Lower back pain","Minor Active",""),
+  (9476719931,"Inactive",'1958-08-04',null,"Fracture of Clavicle","Minor Past","Fell off bicycle"),
+  (9476719931,"Inactive",'1978-09-03',null,"Conjunctivitus","Minor Past","");
 
 INSERT INTO gpconnect.referrals
   (nhsNumber,sectionDate,referral_from,referral_to,priority,details,lastUpdated)
 VALUES
-  (9476719931,'2015-05-01 00:00:01','Airedale Hospital','Dr Johnson &amp; Partners','Routine','From Hospital out-patient for Diabetic Medicine<br />Receiving care','2016-07-25 12:00:00'),
-  (9476719931,'2016-07-01 00:00:01','Dr Johnson &amp; Partners','Leeds District Nurses','Routine','Referral to local authority weight management programme<br />Waiting For Information','2016-07-25 12:00:00');
+  (9476719931,'2016-07-02','Dr Johnson and Partners','Community Diabetic Clinic','Routine','Needs further support with dietary needs','2016-07-02');

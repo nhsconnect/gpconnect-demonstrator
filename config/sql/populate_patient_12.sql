a524a47eac17eb8b0f33908d08da6d8874dc7be3
Saurabh Jain <saurabh.k.jain.1991@gmail.com>
2018-05-15 10:25:59 +0100

    1.1.1 - Issue 60 - Reverted the unnecessary sql changes. Updated the java code instead.

--- config/sql/populate_patient_12.sql
+++ config/sql/populate_patient_12.sql
@@ -1,8 +1,8 @@
 INSERT INTO gpconnect.allergyintolerance
   (nhsNumber,endDate,endReason,note,reactionDescription, clinicalStatus,verificationStatus,category,patientRef,onSetDateTime,assertedDate,coding,display,manCoding,manDisplay, recorder)
 VALUES
-  (9476718927,'2016-07-01 12:00:00',"Cured","Swollen lips, tongue, eyes","Major", "resolved","unconfirmed","medication","2",'2016-05-01 12:00:00','2016-06-01 12:00:00',"886921000000105","Amoxicillin allergy (disorder)","703630003","Red eye (finding)", '9476718927'),
-  (9476718927,'2016-07-01 12:00:00',"Ongoing","Wheezing, chest tightness, shortness of breath","Major", "active","unconfirmed","medication","1",'2016-05-01 12:00:00','2016-06-01 12:00:00',"886921000000105","Salicylate allergy (disorder)","23924001","Tight chest (finding)", '9476718927');
+  (9476718927,'2016-07-01 12:00:00',"Cured","Swollen lips, tongue, eyes","Major", "resolved","unconfirmed","medication","2",'2016-05-01 12:00:00','2016-06-01 12:00:00',"294505008","Amoxicillin allergy (disorder)","703630003","Red eye (finding)", '9476718927'),
+  (9476718927,'2016-07-01 12:00:00',"Ongoing","Wheezing, chest tightness, shortness of breath","Major", "active","unconfirmed","medication","1",'2016-05-01 12:00:00','2016-06-01 12:00:00',"293585002","Salicylate allergy (disorder)","23924001","Tight chest (finding)", '9476718927');
 
   INSERT INTO gpconnect.medication_statements
   (id,lastIssueDate,encounterId,statusCode,statusDisplay,medicationId,startDate,endDate,dateAsserted,

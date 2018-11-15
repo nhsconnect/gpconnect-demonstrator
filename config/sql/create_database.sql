e7f6be47ed15db0ec2c08da99a1709a59a17fd35
lucywilcock <lucy.wilcock@answerdigital.com>
2018-11-15 13:41:45 +0000

    Password correction

--- config/sql/create_database.sql
+++ config/sql/create_database.sql
@@ -1,12 +1,12 @@
 DROP DATABASE IF EXISTS gpconnect;
 CREATE DATABASE gpconnect DEFAULT CHARACTER SET utf8;
 
-/* Delete the answer user (grant all to workaround MySQL not supporting 'IF EXISTS' for users) */
-GRANT ALL ON gpconnect.* TO 'gpconnectdbuser' IDENTIFIED BY 'answer99q';
+/* Delete the user (grant all to workaround MySQL not supporting 'IF EXISTS' for users) */
+GRANT ALL ON gpconnect.* TO 'gpconnectdbuser' IDENTIFIED BY 'gpc0nn3ct';
 DROP USER 'gpconnectdbuser';
 FLUSH PRIVILEGES;
 
-/* Create a new answer user with full privileges */
+/* Create a new user with full privileges */
 CREATE USER 'gpconnectdbuser' IDENTIFIED BY 'gpc0nn3ct';
 GRANT ALL ON gpconnect.* TO 'gpconnectdbuser'@'%' IDENTIFIED BY 'gpc0nn3ct';
 GRANT ALL ON gpconnect.* TO 'gpconnectdbuser'@'localhost' IDENTIFIED BY 'gpc0nn3ct';

d4559c113b947497224e1d68b9e6a12ff9bda52d
Simon Farrow <simon.farrow1@nhs.net>
2022-11-29 08:33:11 +0000

    Fix for #381 Non existent appointment error

--- gpconnect-database/src/main/java/uk/gov/hscic/appointment/appointment/AppointmentSearch.java
+++ gpconnect-database/src/main/java/uk/gov/hscic/appointment/appointment/AppointmentSearch.java
@@ -2,35 +2,39 @@ package uk.gov.hscic.appointment.appointment;
 
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hscic.model.appointment.AppointmentDetail;
-import uk.gov.hscic.appointment.appointment.AppointmentEntity;
-import uk.gov.hscic.appointment.appointment.AppointmentRepository;
 
 @Service
 public class AppointmentSearch {
+
     private final AppointmentEntityToAppointmentDetailTransformer transformer = new AppointmentEntityToAppointmentDetailTransformer();
 
     @Autowired
     private AppointmentRepository appointmentRepository;
 
     public AppointmentDetail findAppointmentByID(Long id) {
-        final AppointmentEntity item = appointmentRepository.findById(id).get();
+        AppointmentEntity item = null;
+        try {
+            item = appointmentRepository.findById(id).get();
+        } catch (NoSuchElementException ex) {
+        }
 
         return item == null
                 ? null
                 : transformer.transform(item);
     }
-    
+
     public AppointmentDetail findAppointmentByIDAndLastUpdated(Long id, Date lastUpdated) {
         final AppointmentEntity item = appointmentRepository.findOneByIdAndLastUpdated(id, lastUpdated);;
 
         return item == null
                 ? null
                 : transformer.transform(item);
-    }  
+    }
 
     public List<AppointmentDetail> searchAppointments(Long patientId, Date startLowerDate, Date startUpperDate) {
         return appointmentRepository.findByPatientId(patientId)

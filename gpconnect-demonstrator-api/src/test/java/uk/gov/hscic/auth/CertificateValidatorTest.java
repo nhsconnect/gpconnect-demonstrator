1597d881b208ca524d8073e1f7ff992d5f0a7546
jasonpittock <jase.pittock@hotmail.co.uk>
2025-11-12 10:07:16 +0000

    critical CVE upgrade fixes

--- gpconnect-demonstrator-api/src/test/java/uk/gov/hscic/auth/CertificateValidatorTest.java
+++ gpconnect-demonstrator-api/src/test/java/uk/gov/hscic/auth/CertificateValidatorTest.java
@@ -1,21 +1,18 @@
 package uk.gov.hscic.auth;
 
 import java.security.cert.X509Certificate;
-import static org.junit.Assert.assertNotNull;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.x509;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import uk.gov.hscic.common.ldap.EndpointResolver;
 
-@RunWith(SpringRunner.class)
 @TestPropertySource(properties = {
     "gp.connect.provider.routing.filename = providerRouting.json",
     "ldap.context.keystore = ldapKeystore.jks",
